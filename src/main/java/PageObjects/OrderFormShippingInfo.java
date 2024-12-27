1bd5fb2195c75c4cbc3e26adb5db0819daee9b53
preston.fernandes <preston.fernandes@zeuslearning.com>
2024-12-27 15:55:43 +0530

    Shipping Method locators

--- src/main/java/PageObjects/OrderFormShippingInfo.java
+++ src/main/java/PageObjects/OrderFormShippingInfo.java
@@ -1,9 +1,24 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class OrderFormShippingInfo {
 	public WebDriver driver;
 	
+	By commentFiled=By.cssSelector("textarea#delivery_message");
+	By continueButton=By.xpath("//button[@name='confirmDeliveryOption']");
 	
+	public OrderFormShippingInfo(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	public WebElement getCommentField() {
+		return driver.findElement(commentFiled);
+	}
+	
+	public WebElement getContinueButton() {
+		return driver.findElement(continueButton);
+	}
 }
